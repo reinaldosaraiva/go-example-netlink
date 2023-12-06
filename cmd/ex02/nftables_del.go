0597fdccfd937ab28ea01bed261fb4979cf2d04c
Reinaldo Saraiva <reinaldo.saraiva@gmail.com>
2023-12-06 07:50:52 -0300

    Add File go nftables rules

---
+++ cmd/ex02/nftables_del.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"log"
+
+	"github.com/vishvananda/netlink"
+)
+
+func main() {
+	// encontrar interafde de rede qual o ip
+	link, err := netlink.LinkByName("enp0s5")
+	if err != nil {
+		log.Fatal(err)
+	}
+	newAddr, err := netlink.ParseAddr("192.168.1.2/24")
+	if err != nil {
+		log.Fatal(err)
+	}
+	// adicionar ip na interface de rede
+	if err := netlink.AddrDel(link, newAddr); err != nil {
+		log.Fatal(err)
+	}
+
+}
